3077838f29551e5a2eb526b42f0141c2e7cc7fc8
João Mateus <joaomateusabm@yahoo.com.br>
2021-09-26 15:35:03 -0300

    chore: added .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://JhonMt:Y5fDImyHxbivJ790@dowhiletypegraphql.gapqv.mongodb.net/test',
-    ),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     UsersModule,
   ],
   controllers: [],
